7cd1b3b0b2cea954b0f6af0bfaad00eee4781a1b
eugenii <eugenii.cher@gmail.com>
2025-08-28 16:26:46 +0300

    Update mytable.bat

--- just_test/mytable.bat
+++ just_test/mytable.bat
@@ -2,7 +2,7 @@
 chcp 65001 > nul
 
 REM Простой вариант - установим время начала на ближайшие несколько минут
-schtasks /create /tn "TestTaskEvery2Min" /tr "D:\Scr\myscr.bat" /sc minute /mo 2 /st 00:01 /ru SYSTEM
+schtasks /create /tn "TestTaskEvery2Min" /tr "C:\Scr\myscr.bat" /sc minute /mo 2 /st 00:01 /ru SYSTEM
 
 if %errorlevel% equ 0 (
     echo Задача создана успешно! Будет выполняться каждые 2 минуты.
@@ -12,7 +12,7 @@ if %errorlevel% equ 0 (
     echo Ошибка при создании задачи.
     echo Убедитесь, что:
     echo 1. Запускаете от имени администратора
-    echo 2. Путь D:\Scr\myscr.bat существует
+    echo 2. Путь C:\Scr\myscr.bat существует
 )
 
 pause
